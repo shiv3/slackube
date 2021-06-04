ca49b301fe2ade7c58e115f806faec6786711e36
shiv3 <shibazaki_takumi@abema.tv>
2021-06-04 21:24:35 +0900

    WIP

---
+++ app/adapter/k8s/client.go
@@ -0,0 +1,21 @@
+package k8s
+
+import (
+	meta_v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+type K8SClient struct {
+
+}
+
+
+func newClient() (kubernetes.Interface, error) {
+	kubeConfig, err := clientcmd.BuildConfigFromFlags("", clientcmd.RecommendedHomeFile)
+	if err != nil {
+		return nil, err
+	}
+
+	return kubernetes.NewForConfig(kubeConfig)
+}
